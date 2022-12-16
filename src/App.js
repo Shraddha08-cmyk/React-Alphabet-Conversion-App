fc345ca9d51dd751bb431551b154b3a18606254d
Shraddha08-cmyk <shraddhatiwari8175@gmail.com>
2022-12-16 15:09:02 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/NavBar';
+// import Navbar from './components/NavBar';
 import Textarea from './components/TextArea';
+// import About from './components/About';
 // import State from './State';
 
 function App() {
   return (
     <>
-    <Navbar />
-    <div className="container">
-    <Textarea heading="Enter the text to coversion"/>
+    {/* <Navbar /> */}
+    <div className="container1">
+    {/* <About /> */}
+    <Textarea heading="Conversion Area"/>
     {/* <State /> */}
     </div>
     </>
--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,7 +1,8 @@
-import {useState} from 'react'
+import { useState } from 'react'
+import '../components/TextArea.css'
 
 function Textarea(props) {
-    
+
     const handleUpClick = () => {
         // console.log("Onclick Fired");
         let newText = text.toUpperCase();
@@ -27,23 +28,24 @@ function Textarea(props) {
     const [text, setText] = useState('Enter text here');
 
 
-    return(
+    return (
         <>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-           <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
-        </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert To Uppercase</button> &nbsp; &nbsp; &nbsp;
-        <button className="btn btn-primary" onClick={handleLoClick}>Convert To Lowercase</button> &nbsp; &nbsp; &nbsp;
-        <button className="btn btn-primary" onClick={handleClearClick}>Clear Text</button>
-
-        <div className="container">
-            <h1>Text Summary</h1>
-            <p>{text.split(" ").length} Words and {text.length} Character</p>
-            <p>{0.008 * text.split(" ").length} Minutes Read</p>
-            <h3>Preview</h3>
-            <p>{text}</p>
-        </div>
+            <div className="cont">
+                <h1 style={{marginLeft: "150px", color: "white", fontFamily: "Comic Sans MS"}}>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea style={{fontFamily: "Comic Sans MS"}} className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
+                </div>
+                <button style={{marginLeft: "30px"}} className="btn btn-primary" onClick={handleUpClick}>Convert To Uppercase</button> &nbsp; &nbsp; &nbsp;
+                <button style={{marginLeft: "30px"}} className="btn btn-primary" onClick={handleLoClick}>Convert To Lowercase</button> &nbsp; &nbsp; &nbsp;
+                <button style={{marginLeft: "30px"}} className="btn btn-primary" onClick={handleClearClick}>Clear Text</button>
+            </div>
+            <div className="container">
+                <h1>Text Summary</h1>
+                <p>{text.split(" ").length} Words and {text.length} Character</p>
+                <p>{0.008 * text.split(" ").length} Minutes Read</p>
+                <h3>Preview</h3>
+                <p>{text}</p>
+            </div>
         </>
     )
 }
